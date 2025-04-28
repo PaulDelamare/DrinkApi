577b31d4d16188680e1f88e4a9f6abebbab32d00
Paul_Delamare <paulodela2006@gmail.com>
2025-04-28 15:06:44 +0200

    Ajout de la route utilisateur dans le serveur et mise en commentaire des anciennes routes

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 const drinkRoutes = require('./routes/drink.routes');
-
+const userRoutes = require('./routes/user.routes');
 
 db.connect((err) => {
   if (err) throw err;
@@ -14,26 +14,27 @@ db.connect((err) => {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is working 🚀');
-});
+// app.get('/', (req, res) => {
+//   res.send('API is working 🚀');
+// });
 
-app.get('/users', (req, res) => {
-  db.query('SELECT * FROM users', (err, results) => {
-    if (err) throw err;
-    res.json(results);
-  });
-});
+// app.get('/users', (req, res) => {
+//   db.query('SELECT * FROM users', (err, results) => {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+// });
 
-app.post('/users', (req, res) => {
-  const { name } = req.body;
-  db.query('INSERT INTO users (name) VALUES (?)', [name], (err) => {
-    if (err) throw err;
-    res.status(201).send('User added');
-  });
-});
+// app.post('/users', (req, res) => {
+//   const { name } = req.body;
+//   db.query('INSERT INTO users (name) VALUES (?)', [name], (err) => {
+//     if (err) throw err;
+//     res.status(201).send('User added');
+//   });
+// });
 
 app.use('/drink', drinkRoutes);
+app.use('/', userRoutes);
 
 app.get('/hello', (req, res) => {
   res.send('Hello World');
