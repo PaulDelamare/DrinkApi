af031efb487ef259f93d11ddf1c7edbc8335407a
Paul_Delamare <paulodela2006@gmail.com>
2025-04-28 14:51:15 +0200

    Refactor la connexion à la base de données pour utiliser une variable de connexion promise

--- api/config/api-config.js
+++ api/config/api-config.js
@@ -2,9 +2,14 @@
 const mysql = require('mysql2');
 
 
-exports.db = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || 'root',
     database: process.env.DB_NAME || 'appdb'
 });
+
+const db = connection.promise();
+
+module.exports = { db };
+
